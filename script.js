b9b83b38045ee7755b85a1093f6e4a5deb142622
Kaio Diniz <kaioigordinizz@hotmail.com>
2024-08-29 00:00:10 -0300

    bugfix: corrigindo quebra de layout | botão download

--- script.js
+++ script.js
@@ -24,17 +24,17 @@ document.getElementById('imageInput').addEventListener('change', function (event
                             const croppedImage = new Image();
                             croppedImage.src = croppedCanvas.toDataURL();
                             croppedImage.onload = function () {
-                                // Desenhar máscara circular
+
                                 ctx.save();
                                 ctx.beginPath();
                                 ctx.arc(frame.width / 2, frame.height / 2, (frame.width - 100) / 2, 0, 2 * Math.PI);
                                 ctx.closePath();
                                 ctx.clip();
 
-                                // Desenhar imagem cortada
+
                                 ctx.drawImage(croppedImage, 50, 50, frame.width - 100, frame.height - 100);
 
-                                // Restaurar contexto para desenhar a moldura sobre a imagem cortada
+
                                 ctx.restore();
                                 ctx.drawImage(frame, 0, 0, frame.width, frame.height);
 
@@ -73,34 +73,34 @@ document.addEventListener('DOMContentLoaded', function () {
             const reader = new FileReader();
             reader.onload = function (e) {
                 image.src = e.target.result;
-                cropButton.style.display = 'block'; // Exibe o botão de Recortar Imagem
-                customButton.style.display = 'none'; // Oculta o botão de Selecionar Arquivo
-                exemploImage.style.display = 'none'; // Oculta a imagem de exemplo
+                cropButton.style.display = 'block';
+                customButton.style.display = 'none';
+                exemploImage.style.display = 'none';
             };
             reader.readAsDataURL(file);
         }
     });
 
     cropButton.addEventListener('click', function () {
-        // Supondo que você já tenha o código para cortar a imagem e desenhá-la no canvas
         const croppedImageDataURL = canvas.toDataURL('image/png');
         resultImage.src = croppedImageDataURL;
         downloadButton.href = croppedImageDataURL;
+        downloadButton.download = 'imagem-cortada.png';
         resultModal.style.display = 'block';
-        cropContainer.style.display = 'none'; // Esconde o crop
-        cropButton.style.display = 'none'; // Esconde o botão de Recortar Imagem
-        customButton.style.display = 'block'; // Exibe o botão de Selecionar Arquivo novamente
+        cropContainer.style.display = 'none';
+        cropButton.style.display = 'none';
+        customButton.style.display = 'block';
     });
 
     closeModal.addEventListener('click', function () {
         resultModal.style.display = 'none';
-        location.reload(); // Recarrega a página
+        location.reload();
     });
 
     window.addEventListener('click', function (event) {
         if (event.target === resultModal) {
             resultModal.style.display = 'none';
-            location.reload(); // Recarrega a página
+            location.reload();
         }
     });
 });
